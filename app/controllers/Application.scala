b0f100f4cc599245c0a3d8cf545b45fc818d2c24
Azula22 <azulazulana@gmail.com>
2016-02-19 18:37:18 +0200

    Prototype: addUser(). Adding works successfully

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,28 @@
 package controllers
 
-import models.PointForm
+import models.{Point, PointForm}
 import play.api.mvc._
 import services.PointService
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class Application extends Controller {
 
   def index = Action.async {
-    implicit request=>
+    implicit request =>
       PointService.listAllPoints map {
-        points=>Ok(views.html.index(PointForm.form, points))
+        points => Ok(views.html.index(PointForm.form, points))
       }
   }
 
-  def addUser = TODO
+  def addUser = Action.async {
+    implicit request =>
+      PointForm.form.bindFromRequest.fold(
+        errorForm => Future.successful(Ok(views.html.index(errorForm, Seq.empty[Point]))),
+        data => {
+          val newPoint = Point(0, data.subject)
+          PointService.addPoint(newPoint).map(res => Redirect(routes.Application.index()))
+        }
+      )
+  }
 }
