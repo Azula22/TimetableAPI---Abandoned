1ebdad9081d1beba87818149f4736accb8b79149
Azula22 <azulazulana@gmail.com>
2016-02-19 17:35:01 +0200

    Prototype: c PointTable

--- app/models/Point.scala
+++ app/models/Point.scala
@@ -2,6 +2,8 @@ package models
 
 import play.api.data.Form
 import play.api.data.Forms._
+import slick.driver.MySQLDriver.api._
+
 
 case class Point(id: BigInt, name: String)
 
@@ -13,4 +15,11 @@ object PointForm{
       "name" -> nonEmptyText
     )(PointFormData.apply)(PointFormData.unapply)
   )
+}
+
+class PointTableDef(tag: Tag) extends Table[Point](tag, "Point"){
+  def id = column[BigInt]("id", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("name")
+
+  override def * = (id, name)<>(Point.tupled, Point.unapply)
 }
