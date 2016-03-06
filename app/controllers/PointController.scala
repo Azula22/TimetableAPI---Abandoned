ee5431f7678d3a0ceb853d1da0af245be697712f
Azula22 <azulazulana@gmail.com>
2016-03-06 18:59:53 +0200

    Changed interface (adding don't work)

--- app/controllers/PointController.scala
+++ app/controllers/PointController.scala
@@ -10,26 +10,22 @@ import scala.concurrent.Future
 class PointController extends Controller {
 
   val days: Seq[String] = Seq("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+val times: Seq[String] = Seq("8:00", "9:35", "11:10", "12:50", "14:25", "16:00")
 
   def index = Action.async {
     implicit request =>
       PointService.listAllPoints map {
-        points => Ok(views.html.index(PointForm.form, points))
+        points => Ok(views.html.index(PointForm.form, points, days, times))
       }
   }
 
-  def addUser = Action.async {
+  def addPoints = Action.async {
     implicit request =>
       PointForm.form.bindFromRequest.fold(
-        errorForm => Future.successful(Ok(views.html.index(errorForm, Seq.empty[Point]))),
+        errorForm => Future.successful(Ok(views.html.index(errorForm, Seq.empty[Point], days, times))),
         data => {
-          if (days.contains(data.day)) {
-            val newPoint = Point(0, data.subject, data.day, data.groupName, data.kind, formatTime(data.start), formatTime(data.ending), data.teacher, data.auditorium)
-            PointService.addPoint(newPoint).map(res => Redirect(routes.PointController.index()))
-          } else Future {
-            //TODO make error message instead redirecting
-            Redirect(routes.PointController.index())
-          }
+          val newPoint = Point(0, data.subject, data.day, data.groupName, data.kind, formatTime(data.start), formatTime(data.ending), data.teacher, data.auditorium)
+          PointService.addPoint(newPoint).map(res => Redirect(routes.PointController.index()))
         }
       )
   }
@@ -45,8 +41,6 @@ class PointController extends Controller {
     val nTime = time + ":00"
     java.sql.Time.valueOf(nTime)
   }
-
-
 
 
 }
