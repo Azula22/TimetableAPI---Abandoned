f524e49a712acf9bae70cc411b8a4017319bc427
Azula22 <azulazulana@gmail.com>
2016-05-02 12:00:49 +0300

    Controller, Service and view for Faculty model

---
+++ app/controllers/FacultyController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+/**
+  * Created by Kushik on 02.05.2016.
+  */
+
+import models.{Faculty, FacultyForm}
+import play.api.mvc._
+import services.FacultyService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class FacultyController extends Controller {
+
+
+  def get() = Action.async {
+    implicit request =>
+      FacultyService.getAllFaculties.map(
+        faculties => Ok(views.html.addFaculty(FacultyForm.form, faculties))
+      )
+  }
+
+  def addFaculty() = Action.async {
+    implicit request =>
+      FacultyForm.form.bindFromRequest.fold(
+        errorForm => Future.successful(Ok(views.html.bad)),
+        data => FacultyService.addFaculty(data.name).map(_ => Redirect(routes.FacultyController.get()))
+      )
+  }
+
+  def deleteFaculty(name: String) = Action.async {
+    implicit request =>
+      FacultyService.deleteFaculty(name).map(_ => Redirect(routes.FacultyController.get()))
+  }
+}
---
+++ app/services/FacultyService.scala
@@ -0,0 +1,23 @@
+package services
+
+import models.{Faculties, Faculty}
+
+import scala.concurrent.Future
+
+/**
+  * Created by Kushik on 02.05.2016.
+  */
+object FacultyService {
+
+  def addFaculty(name: String): Future[String] = {
+    Faculties.add(Faculty(name))
+  }
+
+  def deleteFaculty(name: String): Future[Int] = {
+    Faculties.delete(name)
+  }
+
+  def getAllFaculties: Future[Seq[Faculty]] = {
+    Faculties.listAll
+  }
+}
